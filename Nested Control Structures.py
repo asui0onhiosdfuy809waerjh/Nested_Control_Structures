47ce0473f38e056c64e34960a7b73623c3289a82
Tyler Brunner <02tyler.brunner@gulllakecs.org>
2019-10-23 13:57:26 -0400

    Initial Commit - For Loop + While Loop

--- Nested Control Structures.py
+++ Nested Control Structures.py
@@ -10,3 +10,13 @@ for i in range(3):
     print('Outer For Loop: '+ str(i))
     for l in range(2):
         print("     Inner For Loop: " + str(l))
+
+
+print("\n******************\n")
+
+
+'''
+Programmer: Tyler Brunner
+Date: 10.23.19
+This Program will create a For loop with a While loop embedded into it
+'''
