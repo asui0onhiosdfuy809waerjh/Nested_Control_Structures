cbb2dae3107d1c57346c0938cbb55f9a9f322f5b
Tyler Brunner <02tyler.brunner@gulllakecs.org>
2019-10-23 13:45:54 -0400

    Initial Commit for the Categories Program

--- Nested Control Structures.py
+++ Nested Control Structures.py
@@ -10,3 +10,19 @@ for i in range(3):
     print('Outer For Loop: '+ str(i))
     for l in range(2):
         print("     Inner For Loop: " + str(l))
+
+
+print("\n**********************\n")
+
+
+'''
+Programmer: Tyler Brunner
+Date: 10.23.19
+Program: Categories
+
+This program will ask users of an interest to them
+then ask for two items related to that interest
+'''
+
+
+
